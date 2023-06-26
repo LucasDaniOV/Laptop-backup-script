15582fe7cb305955a105551d9b9f0a76b3c1c7a4
Lucas Oude Vrielink <lucas@oudevrielink.be>
2023-06-26 13:34:49 +0200

    init

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WORK_DIR=/home/private/Projects/laptop_backup_script
+
+source $WORK_DIR/.env
+
+echo "-----STARTING BACKUP @ $(date)-----" >> $LOG_FILE
+
+# Backup installed packages list
+dpkg --get-selections > $WORK_DIR/installed_packages.txt
+rsync -avz -e "ssh -p $SSH_PORT" --progress $WORK_DIR/installed_packages.txt $REMOTE_USER@$REMOTE_HOST:$REMOTE_BACKUP_PATH &>> $LOG_FILE
+
+# Backup local files
+for dir in ${LOCAL_TO_BE_BACKED_UP[@]}; do
+    echo "Backing up $dir @ $(date)" >> $LOG_FILE
+    rsync -avz --delete -e "ssh -p $SSH_PORT" --progress $dir $REMOTE_USER@$REMOTE_HOST:$REMOTE_BACKUP_PATH &>> $LOG_FILE
+    echo "Backup of $dir completed @ $(date)" >> $LOG_FILE
+done
+
+echo "-----BACKUP COMPLETED @ $(date)-----" >> $LOG_FILE
